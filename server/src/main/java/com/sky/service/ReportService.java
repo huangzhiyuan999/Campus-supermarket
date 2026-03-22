c462274af315b175e0976de68ca23c6604af7699
sun123 <3189509410@qq.com>
2026-03-22 11:35:23 +0800

    导出运营数据报表

--- server/src/main/java/com/sky/service/ReportService.java
+++ server/src/main/java/com/sky/service/ReportService.java
@@ -5,6 +5,8 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -39,4 +41,9 @@ public interface ReportService {
      */
     SalesTop10ReportVO getSalesTop10(LocalDate begin,LocalDate end);
 
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    void exportBusinessData(HttpServletResponse response);
 }
