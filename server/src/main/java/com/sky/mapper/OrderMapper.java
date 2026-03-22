ecd4dfa93a01e3f27a092023b9e7d1883bc5202e
sun123 <3189509410@qq.com>
2026-03-22 22:22:16 +0800

    订单状态定时处理

--- server/src/main/java/com/sky/mapper/OrderMapper.java
+++ server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,10 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 插入订单数据
@@ -9,4 +13,14 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface OrderMapper {
     void insert(Orders orders);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
 }
