25755741fb69c490e4dbc3d28724817bcf5f4aeb
sun123 <3189509410@qq.com>
2026-03-22 18:36:20 +0800

    用户数据统计功能代码开发

--- server/src/main/java/com/sky/mapper/UserMapper.java
+++ server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
     /**
@@ -13,4 +15,9 @@ public interface UserMapper {
     User getByOpenid(String openid);
 
     void insert(User user);
+
+    /**
+     * 根据动态条件统计用户数量
+     */
+    Integer countByMap(Map map);
 }
